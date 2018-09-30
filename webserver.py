238ebe63787ceb8c00be7c76e2d544a672b5cb38
Sushen Biswas <sushenbiswasaga@gmail.com>
2018-09-30 14:44:22 +0600

    add user is working.

--- webserver.py
+++ webserver.py
@@ -2,50 +2,113 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from data_map import Base, UserInfo, UserAddress
+engine = create_engine('sqlite:///father.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+session.query(UserInfo).all()
+session.query(UserAddress).all()
+
 class webserverHandler (BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            if self.path.endswith('/hello'):
+            if self.path.endswith("/"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html>"
+                output += "<h1>Welcome</h1>"
+                output += "<a href='/user'>Go to All Users</a>"
+                output += "</br>"
+                output += "</body></html>"
+                self.wfile.write(bytes(output, 'utf-8'))
+                return
+
+            if self.path.endswith('/user'):
                 self.send_response(200)
                 self.send_header('Content-type','text/html')
                 self.end_headers()
 
-                output = b""
-                output += b"<html>"
-                output += b"<h1>Welcome</h1>"
-                output += b"<a href="
-                output += b"/"
-                output += b">Go to WELCOME</a>"
-                # output += b"<html>"
-                # output += b"<h1>Hello</h1>"
-                # output += b"</html>"
-                # output += b"<html><body>"
-                output += b"<h1>HELLO</h1>"
-                output += b'''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += b"</body></html>"
-                self.wfile.write(output)
-                #self.wfile.write(output)#"<body><h1>This is a test.</h1></body>")
+                output = ""
+                output += "<html><body>"
+                output += "<h1> All Our Registerd User</h1>"
+                output += "</br>"
+                output += "<h3> <a href ='/user/new'> Add New User</a></h3>"
+
+                output += "</br>"
+
+
+                users = session.query(UserInfo).all()
+                for user in users:
+                    output += str(user.id)
+                    output += "</br>"
+                    output += str(user.email)
+                    output += "<br/>"
+                    output += "<a href = '/user/edit'> Edit </a>"
+                    output += "<br/>"
+                    output += "<a href = '/user/delete'> Delete </a>"
+                    output += "</br></br>"
+
+
+                output += "</body></html>"
+
+                self.wfile.write(bytes(output, 'utf-8'))
                 print(output)
                 return
 
-            if self.path.endswith('/'):
+            if self.path.endswith('/user/new'):
+                users = session.query(UserInfo).all()
                 self.send_response(200)
                 self.send_header('Content-type','text/html')
                 self.end_headers()
 
-                output =b""
-                output += b"<html>"
-                output += b"<h1>Welcome</h1>"
-                output += b"<a href="
-                output += b"/hello"
-                output += b">Go to Hello</a>"
-                # output += b"</html>"
-                # output += b""
-                # output += b"<html><body>"
-                output += b"<h1>&#161 Hola !</h1>"
-                output += b'''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += b"</body></html>"
-                self.wfile.write(output)
+                output = ""
+                output += "<html><body>"
+
+                output += "<h2>New User form</h1>"
+                output += "<h4><a href = '/user'> See all User </a></h4>"
+
+                output += '''<form method='POST' enctype='multipart/form-data' action='/user/new'><h2>Enter the user email address</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+
+                output += "</body></html>"
+                self.wfile.write(bytes(output, 'utf-8'))
+                print(output)
+                return
+
+            if self.path.endswith('/user/edit'):
+                users = session.query(UserInfo).all()
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+
+                output += "<h2>Edit User </h1>"
+
+                output += "</body></html>"
+                self.wfile.write(bytes(output, 'utf-8'))
+                print(output)
+                return
+
+            if self.path.endswith('/user/delete'):
+                users = session.query(UserInfo).all()
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+
+                output += "<h2>Delete User </h1>"
+
+                output += "</body></html>"
+                self.wfile.write(bytes(output, 'utf-8'))
                 print(output)
                 return
 
@@ -56,22 +119,37 @@ class webserverHandler (BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = b""
-            output += b"<html><body>"
-            output += b" <h2> Okay, how about this: </h2>"
-            output += b"<h1> %s </h1>" % messagecontent[0]
-            output += b'''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += b"</body></html>"
-            self.wfile.write(output)
-            print (output)
+            if self.path.endswith("/user/new"):
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+                pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('message')
+
+                output = ""
+                output += "<html><body>"
+                output += "<h4><a href = '/user'> See all User </a></h4>"
+
+                output += " <h2> You Add a new user and the mail address is: </h2>"
+
+                newUser = UserInfo(email=messagecontent[0])
+                session.add(newUser)
+                session.commit()
+
+                output += "<h1> %s </h1>" % messagecontent[0]
+
+                output += '''<form method='POST' enctype='multipart/form-data' action='/user/new'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+
+                output += "</body></html>"
+
+                self.wfile.write(bytes(output, 'utf-8'))
+                #print (output)
+                return
+
         except:
             pass
 
@@ -79,7 +157,7 @@ def main():
     try:
         port = 8000
         server = HTTPServer(('',port), webserverHandler)
-        print("web server poet %s" %port)
+        print("web server uri http://localhost:%s" %port)
         server.serve_forever()
 
 
